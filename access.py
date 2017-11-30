de006a19c68db31d7468648170b15ebe7563f086
brandon.corfman <bcorfman@fastmail.fm>
2017-11-30 08:27:07 -0500

    Adding code comments

--- access.py
+++ access.py
@@ -21,16 +21,21 @@ class PointBounds(AccessObj):
         self.zones = []
 
     def hide(self):
+        """ Hides the object from the 3D display, but doesn't delete it. """
         self.plotter.outline.visible = False
         for z in self.zones:
             z.visible = False
         self.zones = []
 
     def display(self, pid, extent, mun_az, mun_aof, comp_ids, frag_zones):
+        """ Outlines the burstpoint on the 3D display and displays associated frag zones that intersect with the
+        frag AV components. """
+        # Hides the currently selected object first.
         if self.plotter.access_obj is not None:
             self.plotter.access_obj.hide()
         model = self.plotter.model
         x_min, x_max, y_min, y_max, z_min, z_max = extent
+        # Center the outline around the X,Y,Z midpoint
         self.x_mid = (x_max - x_min) / 2.0 + x_min
         self.y_mid = (y_max - y_min) / 2.0 + y_min
         self.z_mid = (z_max - z_min) / 2.0 + z_min
@@ -42,26 +47,27 @@ class PointBounds(AccessObj):
                                        self.z_mid - 0.5, self.z_mid + 0.5)
         self.plotter.outline.visible = True
 
-        # add each steradian (sphere slice representing a zone) to a single AppendPolyData object, and then
-        # add the PolyData object to the Mayavi pipeline.
+        # set up a Transform to rotate and translate each frag zone steradian to the correct position at the end.
         t = tvtk.Transform()
         # transforms occur in reverse order (The Visualization Toolkit 4th ed, p. 73)
         t.translate(self.x_mid, self.y_mid, self.z_mid)
         t.rotate_z(mun_az)
-        t.rotate_y(-90.0 + mun_aof)
+        t.rotate_y(-90.0 + mun_aof)  # AOF - 90 is necessary to give correct orientation in the Mayavi coord sys
         t.rotate_x(180.0)  # 0 deg is at North Pole by default, so must flip 180 deg to match JMAE orientation
+
         # create reverse lookup -- all component ids that use each zone angle
         zone_lookup = collections.defaultdict(list)
         for cid in comp_ids:
             zones = frag_zones[pid][mun_az][cid]
             for _, lower_angle, upper_angle in zones:
                 zone_lookup[(lower_angle, upper_angle)].append(cid)
+
         # display each frag zone using largest radius of involved components
         max_radius = 0.0
         for zone in zone_lookup:
             lower_angle, upper_angle = zone
             for cid in zone_lookup[zone]:
-                sphere_radius = self.dist_to_active_comps(cid)
+                sphere_radius = self.dist_to_active_comp(cid)
                 max_radius = max(sphere_radius, max_radius)
                 # set the center to (0, 0, 0) so rotation occurs about the origin first, then translate at the end.
                 frag_zone = tvtk.SphereSource(center=(0, 0, 0), radius=sphere_radius,
@@ -71,29 +77,22 @@ class PointBounds(AccessObj):
                 zone_obj.update()
                 zone_tf = tvtk.TransformPolyDataFilter(input_connection=zone_obj.output_port, transform=t)
                 zone_tf.update()
+                # add the surface to the Mayavi scene
                 surf = self.plotter.scene.mlab.pipeline.surface(zone_tf.output, reset_zoom=False)
+                # manually adjust zone color according to PK value. When the 3D scene was generated, a color lookup
+                # table was saved when the matrix plot was done, and here I use it to match the same color scheme.
                 tid = self.plotter.lut_table.get_index(model.comp_pk[pid][mun_az][cid])
                 surf.actor.actor.property = tvtk.Property(opacity=0.5,
                                                           color=self.plotter.lut_table.get_table_value(tid)[0:3])
+                # store all the zones in a list so we can do operations (like hide) on them later.
                 self.zones.append(surf)
                 surf.actor.update_data()
 
-        # TODO: figure out why this method of removing sphere triangles below the matrix doesn't work.
-        # TODO: When I use this code, I don't see anything displayed.
-        # x_len = abs(model.mtx_extent_range[0] - model.mtx_extent_range[1])
-        # y_len = abs(model.mtx_extent_defl[0] - model.mtx_extent_defl[1])
-        # cube = tvtk.CubeSource(center=(model.offset_range+model.tgt_center[0], model.offset_defl+model.tgt_center[1],
-        #                                (model.burst_height - max_radius) / 2.0),
-        #                        x_length=x_len, y_length=y_len, z_length=max_radius)
-        # tri1 = tvtk.TriangleFilter(input_connection=zone_obj.output_port)
-        # tri2 = tvtk.TriangleFilter(input_connection=cube.output_port)
-        # boolean_op = tvtk.BooleanOperationPolyDataFilter()
-        # boolean_op.operation = 'union'
-        # boolean_op.add_input_connection(0, tri1.output_port)
-        # boolean_op.add_input_connection(1, tri2.output_port)
-        # boolean_op.update()
-
-    def dist_to_active_comps(self, idx):
+    def dist_to_active_comp(self, idx):
+        """ Distance between burstpoint and AV component.
+        :param idx: AV component index
+        :return: distance distance between the burstpoint X,Y,Z coordinates and the AV component X,Y,Z coordinates.
+        """
         model = self.plotter.model
         cmp_x, cmp_y, cmp_z = model.comps[idx].x, model.comps[idx].y, model.comps[idx].z
         return sqrt((self.x_mid - cmp_x) ** 2 + (self.y_mid - cmp_y) ** 2 + (self.z_mid - cmp_z) ** 2)
@@ -116,10 +115,12 @@ class CellBounds(AccessObj):
         self.hide()
 
     def hide(self):
+        """ Hides the outline and callout from the 3D display, but doesn't delete it. """
         self.plotter.outline.visible = False
         self.callout.visible = False
 
     def display(self, extent, pk):
+        """ Outlines the selected cell on the 3D display and displays the cell PK value in a callout. """
         if self.plotter.access_obj is not None:
             self.plotter.access_obj.hide()
         x_min, x_max, y_min, y_max, z_min, z_max = extent
